1b9526cd27e3713a84089ba0aa502bb5fe7a39e6
Kartik Sharma <85084155+KartikSharma0@users.noreply.github.com>
2022-12-05 20:50:55 -0700

    Update Email.java

--- Email.java
+++ Email.java
@@ -1,53 +1,61 @@
-import java.util.*;
-import javax.mail.*;
-import javax.mail.internet.*;
-import javax.activation.*;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 
 public class Email {
-    private String recipient = "";
-    private String sender = "";
-    private String host = "localhost";
-    private Properties properties = System.getProperties();
-    private Session session;
 
+    public static void sendEmail(String to)
+    {
+        final String username = "testingschoolprojects@outlook.com";
+        final String password = "testemail123";
 
-    public Email(String recipient, String sender){
-        this.recipient = recipient;
-        this.sender = sender;
-        properties.setProperty("mail.smtp.host", host);
-        session = Session.getDefaultInstance(properties);
-    }
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp-mail.outlook.com");
+        props.put("mail.smtp.port", "587");
 
-    
-    public void sendEmail(){
-        try{
-            MimeMessage message = new MimeMessage(session);
+        Session session = Session.getInstance(props,
+          new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+          });
 
-            // Set From: header field of the header.
-            message.setFrom(new InternetAddress(sender));
+        try {
 
-            // Set To: header field of the header.
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("testingschoolprojects@outlook.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                InternetAddress.parse(to));
+            message.setSubject("A testing mail header !!!");
+            message.setText("Dear Mail Crawler,"
+                + "\n\n No spam to my email, please!");
 
-            // Set Subject: header field
-            message.setSubject("This is the Subject Line!");
-
-            // Send the actual HTML message, as big as you like
-            message.setContent("<h1>This is actual message</h1>", "text/html");
-
-            // Send message
             Transport.send(message);
-            System.out.println("Sent message successfully....");
-        } catch(MessagingException e){
+
+            System.out.println("Done");
+
+        } 
+
+        catch (MessagingException e) 
+        {
+            // throw new RuntimeException(e);
             e.printStackTrace();
         }
-
     }
 
-    public static void main(String[] args) {
-        Email email = new Email("kirtan.kakadiya@gmail.com", "kirtan.kakadiya@gmail.com");
 
-        email.sendEmail();
+    public static void main(String[] args) 
+    {
+        String to = "testingschoolprojects@outlook.com";
+        sendEmail(to);
     }
-    
+
 }
